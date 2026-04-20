700904f364b10ce8e1f51f7f4dd2c5ff8228dd1f
BillerPlay <abulyaa@gmail.com>
2026-04-20 16:23:58 +0400

    Initial commit - Restoran Management System

---
+++ src/main/java/com/ironhack/restoranmanagementsystem/RestoranManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.restoranmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestoranManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestoranManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ironhack/restoranmanagementsystem/RestoranManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.restoranmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoranManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
